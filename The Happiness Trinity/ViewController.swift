c166c85d8637bdebbd70445cb9ba3db24ddd19ab
sky121 <skylarooz@gmail.com>
2021-01-29 20:56:57 -0800

    Initial Commit

---
+++ The Happiness Trinity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Happiness Trinity
+//
+//  Created by Sky H on 1/29/21.
+//  Copyright © 2021 Sky H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ The Happiness TrinityTests/The_Happiness_TrinityTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_Happiness_TrinityTests.swift
+//  The Happiness TrinityTests
+//
+//  Created by Sky H on 1/29/21.
+//  Copyright © 2021 Sky H. All rights reserved.
+//
+
+import XCTest
+@testable import The_Happiness_Trinity
+
+class The_Happiness_TrinityTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
