3772975b453e022ed379b95ce12dac8db659e602
Jingwen Li <jingwl13@uci.edu>
2021-01-30 12:49:29 -0800

    pushing the journal icon and new image view controller

---
+++ The Happiness Trinity/ImageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageViewController.swift
+//  The Happiness Trinity
+//
+//  Created by Jimmy LI on 1/30/21.
+//  Copyright © 2021 Sky H. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
