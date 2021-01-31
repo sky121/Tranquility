f06edf10f43497d558a8fb12970fd19cfccc7b0b
Jingwen Li <jingwl13@uci.edu>
2021-01-30 21:54:45 -0800

    struggling with segue

---
+++ The Happiness Trinity/PictureCollectionViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PictureCollectionViewController.swift
+//  The Happiness Trinity
+//
+//  Created by Jimmy LI on 1/30/21.
+//  Copyright © 2021 Sky H. All rights reserved.
+//
+
+import UIKit
+
+class PictureCollectionViewController: UIViewController {
+
+    
+    var picture_gallery: [[String:Any]]!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("I am in pictures collections")
+        print(picture_gallery)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
