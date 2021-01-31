9e50e71eab786e9de9e8ece9e4d725c5d8b855c4
jacobjh1 <jacobjh1@uci.edu>
2021-01-31 01:17:51 -0800

    colors in calendar

---
+++ The Happiness Trinity/AccompCell.swift
@@ -0,0 +1,26 @@
+//
+//  AccompCell.swift
+//  The Happiness Trinity
+//
+//  Created by Jacob_Huang on 1/31/21.
+//  Copyright © 2021 Sky H. All rights reserved.
+//
+
+import UIKit
+
+class AccompCell: UITableViewCell {
+
+    @IBOutlet weak var accompLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
