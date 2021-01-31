5701b0662e55bbf1a556284cb3084d919e34703a
Jingwen Li <jingwl13@uci.edu>
2021-01-31 01:16:18 -0800

    dog images are working

---
+++ The Happiness Trinity/PictureViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PictureViewCell.swift
+//  The Happiness Trinity
+//
+//  Created by Jimmy LI on 1/30/21.
+//  Copyright © 2021 Sky H. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var picture: UIImageView!
+}
